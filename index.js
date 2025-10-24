7cebfa52806fc8938bf97382b8f11736ff53c3f9
Nicholas Lam <100057820+Nicholas-Lam1@users.noreply.github.com>
2025-10-24 17:35:35 -0600

    Calculator functional, only for operations between two numbers. Brackets not functional.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let equation = {
+  firstInput: '',
+  operator: '',
+  secondInput: ''
+};
+let currentInput = '';
+let result = null; 
+
 function add(a, b) {
   return a + b;
 }
@@ -26,7 +34,157 @@ function sqrt(a) {
   return a ** (1/2);
 }
 
+function updateDisplay() {
+  const typetile = document.getElementById("typetile");
+  typetile.textContent = currentInput;
+  
+  const equationtile = document.getElementById("equationtile");
+  equationtile.textContent = Object.values(equation).join(' ')
+}
 
+function calculate() {
+  const a = parseFloat(equation.firstInput)
+  const b = parseFloat(equation.secondInput)
+  switch (equation.operator) {
+    case "+":
+      currentInput = add(a, b).toString();
+      updateDisplay();
+      break;
+    case "-":
+      currentInput = subtract(a, b).toString();
+      updateDisplay();
+      break;
+    case "*":
+      currentInput = multiply(a, b).toString();
+      updateDisplay();
+      break;
+    case "/":
+      currentInput = divide(a, b).toString();
+      updateDisplay();
+      break;
+    case "mod":
+      currentInput = mod(a, b).toString();
+      updateDisplay();
+      break;
+    case "**":
+      currentInput = exp(a, b).toString();
+      updateDisplay();
+      break;
+  }
+}
+
+function handleInput(e) {
+  const target = e.target.id;
+  const value = e.target.innerText;
+
+  switch (target) {
+    case "cleartile":
+      if (currentInput !== '') {
+        currentInput = '';
+      }
+      else {
+        equation.firstInput = '';
+        equation.operator = '';
+        equation.secondInput = '';
+      }
+      updateDisplay();
+      break;
+
+    case "onetile":
+    case "twotile":
+    case "threetile":
+    case "fourtile":
+    case "fivetile":
+    case "sixtile":
+    case "seventile":
+    case "eighttile":
+    case "ninetile":
+      if (equation.secondInput === '') {
+        currentInput += (String(value));
+      }
+      else {
+        equation.firstInput = '';
+        equation.operator = '';
+        equation.secondInput = '';
+        currentInput = (String(value));
+      }
+      updateDisplay();
+      break;
+
+    case "zerotile":
+      if (currentInput !== '') {
+        currentInput += value;
+        updateDisplay(currentInput);
+      }
+      break;
+
+    case "decitile":
+      if (!currentInput.includes('.')) {
+        currentInput += value; // Add decimal point if not already present
+        updateDisplay(currentInput);
+      }
+      break;
+
+    case "addtile":
+    case "minustile":
+    case "multtile":
+    case "divtile":
+    case "modtile":
+    case "exptile":
+      if (currentInput === '') {
+        return;
+      }
+      equation.operator = value;
+      if(equation.firstInput === '') {
+        equation.firstInput = currentInput;
+      }
+      currentInput = '';
+      updateDisplay();
+      break;
+
+    case "equaltile":
+      if (equation.operator !== '' && currentInput !== '') {
+        equation.secondInput = currentInput;
+        console.log(equation.firstInput, equation.operator, equation.secondInput)
+        calculate();
+      }
+      break;
+
+    case "percenttile":
+      if (currentInput !== '') {
+        currentInput = (parseFloat(currentInput) / 100).toString();
+        updateDisplay(currentInput);
+      }
+      break;
+
+    case "sqrttile":
+      if (currentInput !== '') {
+        equation.operator = value;
+        equation.secondInput = currentInput;
+        currentInput = sqrt(parseFloat(currentInput)).toString();
+        updateDisplay();
+      }
+      break;
+
+    case "backtile":
+      currentInput = currentInput.length > 1 ? currentInput.slice(0, -1) : currentInput = '';
+      updateDisplay(currentInput);
+      break;
+
+    case "closetile":
+      currentInput += '(';
+      updateDisplay(currentInput);
+      break;
+
+    case "opentile":
+      currentInput += ')';
+      updateDisplay(currentInput);
+      break;
+
+    default:
+      break;
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   (() => {
@@ -39,11 +197,8 @@ document.addEventListener("DOMContentLoaded", () => {
 function initCalc(){
   const buttons = document.querySelectorAll(".calc-btn");
 
-  // Handle clicks
   buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      handleInput(button.id);
-    });
+    button.addEventListener("click", handleInput);
   });
 }
 
